f5caf5a999f84318544e9b5b5208e3b7adce287b
uzlopak <aras.abbasi@googlemail.com>
2023-10-26 13:52:17 +0200

    fix

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,10 @@
-import { FastifyRequest, preHandlerAsyncHookHandler } from "fastify";
+import { FastifyRequest } from "fastify";
 import { Unauthorized } from "http-errors";
+import { PostBody } from "./schemas/post/body";
 
-export const preHandler: preHandlerAsyncHookHandler = async (req: FastifyRequest) => {
+export const preHandler = async (req: FastifyRequest<{
+  Body: PostBody;
+}>) => {
   const rawBody = req.rawBody;
   if (rawBody === undefined) throw Unauthorized("No raw body on the request");
   console.log("Pre handler executed")
